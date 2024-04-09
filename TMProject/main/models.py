1c716830db8071de9cf88bd3840995411db7d49d
Pavel Umanskiy <114307635+PavelUmanskiy@users.noreply.github.com>
2024-04-09 21:20:58 +0300

    Add models

--- TMProject/main/models.py
+++ TMProject/main/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Menu(models.Model):
+    name = models.CharField(max_length=128, default='Default Menu Name')
+
+
+class MenuNode(models.Model):
+    menu = models.ForeignKey(
+            'Menu',
+            on_delete=models.CASCADE,
+            related_name='nodes'
+    )
+    display_name = models.CharField(max_length=128, default='Default Node')
+    url = models.URLField(default='/')
+    children = models.ManyToManyField('MenuNode', related_name='parent')
+    
+    def __str__(self) -> str:
+        return self.display_name
+    
